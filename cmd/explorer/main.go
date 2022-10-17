f2df9af1d364ed743c9e40edc0555784e5886ad6
Chris Hewison <hewisonchris@gmail.com>
2022-10-17 11:28:35 +0900

    Correct website url

--- cmd/explorer/main.go
+++ cmd/explorer/main.go
@@ -230,7 +230,7 @@ func main() {
 
 		if !utils.Config.Frontend.OnlyAPI {
 			if utils.Config.Frontend.SiteDomain == "" {
-				utils.Config.Frontend.SiteDomain = "agora.in"
+				utils.Config.Frontend.SiteDomain = "www.agorascan.io"
 			}
 
 			logrus.Infof("frontend database connection established")
